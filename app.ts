7d5dedd21ca1d2ec502584051aba7cdb8016eac6
tosin <tohshine@gmail.com>
2025-01-21 19:50:54 +0000

    done1

--- app.ts
+++ app.ts
@@ -3,4 +3,4 @@ import {app} from './src'
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT,()=>log(`server running on port!!!! ${PORT}`))
+app.listen(PORT,()=>log(`server running on port! ${PORT}`))
