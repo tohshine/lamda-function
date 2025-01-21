760bd8b92187f424ca710f1bdd5f1c8f6ec12484
tosin <tohshine@gmail.com>
2025-01-21 19:25:51 +0000

    added test file

---
+++ src/__test__/getAllUsers.test.ts
@@ -0,0 +1,10 @@
+import {app} from '../index'
+import request from 'supertest'
+
+it('respond with list od users',async ()=>{
+ await request(app)
+              .get('/users')
+              .send()
+              .expect(200)
+
+})
